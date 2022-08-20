37a934d57b10838050244ef8d82eb472b8739b47
KamalKech <maroc.develop@gmail.com>
2022-08-20 19:06:28 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,36 +23,69 @@
 <!-- </div> -->
 <!---->
 
-<div class="wrapper mt-20">
-	<div class="w-1/2 m-auto">
-		<div class="biography flex flex-row gap-4">
-			<div class="photo">
-				<img
-					src="./img/kamal.jpg"
-					alt="kamal sahmoud"
-					class="opacity-20"
-					
-				/>
-			</div>
-			<div class="biography">
-				<h1 class="glitch mb-10" id="main-title">
-					<span aria-hidden="true">Kamal Sahmoud</span>
-					Kamal Sahmoud
-					<span aria-hidden="true">Kamal Sahmoud</span>
-				</h1>
-				<h2 class="glitch text-2xl tracking-tight font-extrabold sm:text-5xl md:text-3xl">
-					A fullstack engineer, auto learn and build different stuff for web
-				</h2>
-				<p class="mt-6 text-slate-50">
-					Hello, i am web developer frontend / backend for more than 10 years. I am currently
-					working in a web agency in Rabat. I have acquired know-how and solid knowledge in all
-					stages of website creation. Autonomous and sociable, i can carry out a solo project as
-					well as integrate myself into an already existing team. Flexible on time and pace of work,
-					I adapt to meet your needs and minimize your development costs. My qualities are good
-					listening / understanding of customer needs, seriousness in the realization, advice and
-					decision support before development.
-				</p>
-			</div>
+<!-- <div class="wrapper mt-10"> -->
+<!-- 	<div class="w-1/2 m-auto"> -->
+<!-- 		<div class="photo"> -->
+<!-- 			<img -->
+<!-- 				src="./img/kamal.jpg" -->
+<!-- 				alt="kamal sahmoud" -->
+<!-- 				class="opacity-20" -->
+<!-- 				width="200px" -->
+<!-- 				height="200px" -->
+<!-- 			/> -->
+<!-- 		</div> -->
+<!---->
+<!-- 		<h1 class="glitch" id="main-title"> -->
+<!-- 			<span aria-hidden="true">Kamal Sahmoud</span> -->
+<!-- 			Kamal Sahmoud -->
+<!-- 			<span aria-hidden="true">Kamal Sahmoud</span> -->
+<!-- 		</h1> -->
+<!---->
+<!-- 		<h2 class="glitch text-2xl tracking-tight font-extrabold sm:text-5xl md:text-2xl"> -->
+<!-- 			A fullstack engineer, auto learn and build different stuff for web -->
+<!-- 		</h2> -->
+<!-- 		<p class="mt-6 text-slate-50 text-sm"> -->
+<!-- 			Hello, i am web developer frontend / backend for more than 10 years, currently i am working in -->
+<!-- 			a web agency in Rabat. I have acquired know-how and solid knowledge in all stages of website -->
+<!-- 			creation. Autonomous and sociable, i can carry out a solo project as well as integrate myself -->
+<!-- 			into an already existing team. Flexible on time and pace of work, I adapt to meet your needs -->
+<!-- 			and minimize your development costs. My qualities are good listening / understanding of -->
+<!-- 			customer needs, seriousness in the realization, advice and decision support before -->
+<!-- 			development. -->
+<!-- 		</p> -->
+<!-- 	</div> -->
+<!-- </div> -->
+
+<div class="w-1/2 m-auto mt-10">
+	<div class="grid grid-cols-3 gap-4">
+		<div class="photo">
+			<img
+				src="./img/kamal.jpg"
+				alt="kamal sahmoud"
+				class="opacity-20"
+				width="200px"
+				height="200px"
+			/>
+		</div>
+
+		<div class="bioigraphy col-span-2">
+			<h1 class="glitch uppercase text-3xl mb-8" id="main-title">
+				<span aria-hidden="true">Kamal Sahmoud</span>
+				Kamal Sahmoud
+				<span aria-hidden="true">Kamal Sahmoud</span>
+			</h1>
+
+			<h2 class="glitch normal-case text-2xl tracking-tight font-extrabold sm:text-5xl md:text-2xl">
+				A fullstack engineer, auto learn and build different stuff for web
+			</h2>
 		</div>
 	</div>
+	<p class="mt-6 text-slate-50 text-sm">
+		Hello, i am web developer frontend / backend for more than 10 years, currently i am working in a
+		web agency in Rabat. I have acquired know-how and solid knowledge in all stages of website
+		creation. Autonomous and sociable, i can carry out a solo project as well as integrate myself
+		into an already existing team. Flexible on time and pace of work, I adapt to meet your needs and
+		minimize your development costs. My qualities are good listening / understanding of customer
+		needs, seriousness in the realization, advice and decision support before development.
+	</p>
 </div>
