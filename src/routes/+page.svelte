92535f4f3396c50260dfae6412468a4367bab986
kamalkech <maroc.develop@gmail.com>
2022-08-20 13:32:43 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<!-- <div class="wrapper mt-20 text-lime-400"> -->
+<!-- 	<div class="w-1/2 m-auto"> -->
+<!-- 		<h1 class="glitch mb-10" id="main-title"> -->
+<!-- 			<span aria-hidden="true">Kamal Sahmoud</span> -->
+<!-- 			Kamal Sahmoud -->
+<!-- 			<span aria-hidden="true">Kamal Sahmoud</span> -->
+<!-- 		</h1> -->
+<!-- 		<div class="biography grid grid-rows-2 grid-cols-2 gap-4"> -->
+<!-- 			<div class="photo"> -->
+<!-- 				<img src="./img/kamal.jpg" alt="kamal sahmoud" class="opacity-20" /> -->
+<!-- 			</div> -->
+<!-- 			<div class="biography"> -->
+<!-- 				<h2 class="text-2xl tracking-tight font-extrabold sm:text-5xl md:text-3xl"> -->
+<!-- 					A fullstack engineer, auto learn and build different stuff for web -->
+<!-- 				</h2> -->
+<!-- 				<p class="mt-20"> -->
+<!-- 					A simple, customizable, and, beautiful SaaS business focused landing page to make your -->
+<!-- 					project closer to launch! -->
+<!-- 				</p> -->
+<!-- 			</div> -->
+<!-- 		</div> -->
+<!-- 	</div> -->
+<!-- </div> -->
+<!---->
+
+<div class="wrapper mt-20">
+	<div class="w-1/2 m-auto">
+		<div class="biography flex flex-row gap-4">
+			<div class="photo">
+				<img
+					src="./img/kamal.jpg"
+					alt="kamal sahmoud"
+					class="opacity-20"
+					
+				/>
+			</div>
+			<div class="biography">
+				<h1 class="glitch mb-10" id="main-title">
+					<span aria-hidden="true">Kamal Sahmoud</span>
+					Kamal Sahmoud
+					<span aria-hidden="true">Kamal Sahmoud</span>
+				</h1>
+				<h2 class="glitch text-2xl tracking-tight font-extrabold sm:text-5xl md:text-3xl">
+					A fullstack engineer, auto learn and build different stuff for web
+				</h2>
+				<p class="mt-6 text-slate-50">
+					Hello, i am web developer frontend / backend for more than 10 years. I am currently
+					working in a web agency in Rabat. I have acquired know-how and solid knowledge in all
+					stages of website creation. Autonomous and sociable, i can carry out a solo project as
+					well as integrate myself into an already existing team. Flexible on time and pace of work,
+					I adapt to meet your needs and minimize your development costs. My qualities are good
+					listening / understanding of customer needs, seriousness in the realization, advice and
+					decision support before development.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
