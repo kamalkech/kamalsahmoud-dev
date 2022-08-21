c57f49f9c63ece87733182da49334b16482daf40
kamalkech <maroc.develop@gmail.com>
2022-08-21 20:40:56 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -113,8 +113,9 @@
 	<!-- end skills -->
 
 	<div class="contact glitch mb-6">
-		<div class="title-section h-10 my-6">
+		<div class="title-section my-6">
 			<h2 class="normal-case text-2xl tracking-tight font-extrabold sm:text-2xl md:text-4xl">
+				<img src="/img/phone-17-64.png" alt="tv" />
 				Contact me
 			</h2>
 		</div>
