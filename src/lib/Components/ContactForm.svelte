d8f21bbbfa397834861c5e92300aef2a62cfa62d
kamalkech <maroc.develop@gmail.com>
2022-08-30 23:58:48 +0100

    .

--- src/lib/Components/ContactForm.svelte
+++ src/lib/Components/ContactForm.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import FormContact from '$lib/Components/Form.svelte';
+	import FormContact from '$lib/Components/Form.svelte'
 </script>
 
 <!-- Start contact form -->
 <div class="contact glitch mb-6">
 	<div class="title-section my-6">
 		<h2 class="normal-case text-2xl tracking-tight font-extrabold sm:text-2xl md:text-4xl">
-			<img src="/img/phone-17-64.png" alt="tv" />
 			Contact me
 		</h2>
 	</div>
--- src/lib/util/types.ts
+++ src/lib/util/types.ts
@@ -1,6 +1,10 @@
 export interface ContactInput {
-	fullName: string;
-	email: string;
-	subject: string;
-	message: string;
+	fullName: string
+	email: string
+	subject: string
+	message: string
+}
+
+export interface Person {
+	name: string
 }
